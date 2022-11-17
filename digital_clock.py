b9f788f7a63bd8f2abc07b7476855ed7ce342146
bahram3d <32274049+bahram3d@users.noreply.github.com>
2022-11-16 19:27:52 -0800

    added digital clock to new branch

--- digital_clock.py
+++ digital_clock.py
@@ -1,4 +1,5 @@
 from tkinter import Tk,Label
+from datetime import datetime
 
 window = Tk()
 window.title("Digital Clock")
@@ -7,4 +8,11 @@ window.configure(bg="steelblue")
 
 label = Label(window, text= "Good to see you!", font= ("Arial Black", 70, "bold"), bg = "steelblue",fg="white")
 label.pack(pady=100)
+
+def clock():
+    time =datetime.now().strftime("%H:%M:%S")
+    label.configure(text=time)
+    label.after(500, clock)
+    
+clock()
 window.mainloop()
