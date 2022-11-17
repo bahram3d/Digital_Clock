572b71aaf190c03c2bdad79db0cf3a72f4d5a08f
bahram3d <32274049+bahram3d@users.noreply.github.com>
2022-11-16 19:15:41 -0800

    add bg color and welcome text

--- digital_clock.py
+++ digital_clock.py
@@ -2,5 +2,9 @@ from tkinter import Tk,Label
 
 window = Tk()
 window.title("Digital Clock")
-window.geometry("600x300")
+window.geometry("1000x300")
+window.configure(bg="steelblue")
+
+label = Label(window, text= "Good to see you!", font= ("Arial Black", 70, "bold"), bg = "steelblue",fg="white")
+label.pack(pady=100)
 window.mainloop()
